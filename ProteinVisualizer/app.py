778372f35f8fcabd61820d7cd135f303db708e74
magnite5 <87040030+magnite5@users.noreply.github.com>
2025-11-13 16:45:15 -0500

    Fully Functional Version

--- ProteinVisualizer/app.py
+++ ProteinVisualizer/app.py
@@ -7,6 +7,8 @@ import py3Dmol
 import pandas as pd
 import requests
 
+from analysis_utils import AnalysisUtils
+
 app = Flask(__name__)
 
 ### Helper functions ###
@@ -75,6 +77,7 @@ def home():
     """Render home page with PDB ID input form."""
     return render_template('home.html')
 
+@app.route('/test_view')
 
 @app.route('/view')
 def view_pdb():
@@ -116,171 +119,79 @@ def view_pdb():
         current_inst = df.loc[i]["instability_index"]
         current_length = df.loc[i]["length"]
         count_inst += current_inst * current_length
-    Weigthed_instability_index = count_inst/chain_length
-
+    weighted_instability_index = count_inst/chain_length
 
     count_aromaticity = 0
     for i in range(df.shape[0]):
         current_aromaticity = df.loc[i]["aromaticity"]
         current_length = df.loc[i]["length"]
         count_aromaticity += current_aromaticity * current_length
-    Weigthed_aromaticity = count_aromaticity/chain_length
+    weighted_aromaticity = count_aromaticity/chain_length
 
     count_pI = 0
     for i in range(df.shape[0]):
         current_pI = df.loc[i]["pI"]
         current_length = df.loc[i]["length"]
         count_pI += current_pI * current_length
-    Weigthed_pI = count_pI/chain_length
+    weighted_pi = count_pI/chain_length
 
-
-    def analyse():
-        Strong = 0
-        Moderate = 0
-        Weak = 0
-
-        value = analyse_chain_length()
-        if value == "Strong":
-            Strong += 1
-        elif value == "Moderate":
-            Moderate += 1
-        else:
-            Weak += 1
-
-        value = analyse_molecular_weight()
-        if value == "Strong":
-            Strong += 1
-        elif value == "Moderate":
-            Moderate += 1
-        else:
-            Weak += 1
+    analysis = AnalysisUtils.analyse(
+        chain_length, 
+        molecular_weight, 
+        weighted_aromaticity, 
+        weighted_instability_index, 
+        weighted_pi
+    )
+    
+    chain_length_analysis = AnalysisUtils.analyse_chain_length(chain_length)
+    molecular_weight_analysis = AnalysisUtils.analyse_molecular_weight(molecular_weight)
+    aromaticity_analysis = AnalysisUtils.analyse_aromaticity(weighted_aromaticity)
+    instability_index_analysis = AnalysisUtils.analyse_instability_index(weighted_instability_index)
+    pi_analysis = AnalysisUtils.analyse_pi(weighted_pi)
         
-        value = analyse_aromaticity()
-        if value == "Strong":
-            Strong += 1
-        elif value == "Moderate":
-            Moderate += 1
-        else:
-            Weak += 1
-        
-        value = analyse_instability_index()
-        if value == "Strong":
-            Strong += 1
-        elif value == "Moderate":
-            Moderate += 1
-        else:
-            Weak += 1
-        
-        value = analyse_pI()
-        if value == "Strong":
-            Strong += 1
-        elif value == "Moderate":
-            Moderate += 1
-        else:
-            Weak += 1
-
-
-        if Strong > Moderate and Strong > Weak:
-            return "Strong"
-        elif Moderate > Strong and Moderate > Weak:
-            return "Moderate"
-        elif Weak > Strong and Weak > Moderate:
-            return "Weak"
-        elif Strong == Moderate and Strong > Weak:
-            return "Strong"
-        elif Strong == Weak and Strong > Moderate:
-            return "Strong"
-        elif Strong == Weak and Strong == Moderate:
-            return "Strong"
-        elif Moderate == Weak and Moderate > Strong:
-            return "Moderate"
-        else:
-            return "Strong"
-
-
-    def analyse_chain_length():
-        if chain_length > 400:
-            return "Strong"
-        elif chain_length >= 150 and chain_length <= 400:
-            return "Moderate"
-        else:
-            return "Weak"
-
-    def analyse_molecular_weight():
-        if molecular_weight > 60000:
-            return "Strong"
-        elif molecular_weight >= 20000 and molecular_weight <= 60000:
-            return "Moderate"
-        else:
-            return "Weak"
-
-    def analyse_aromaticity():
-        if Weigthed_aromaticity > 0.10:
-            return "Strong"
-        elif Weigthed_aromaticity >= 0.07 and Weigthed_aromaticity <= 0.10:
-            return "Moderate"
-        else:
-            return "Weak"
-
-    def analyse_instability_index():
-        if Weigthed_instability_index < 30:
-            return "Strong"
-        elif Weigthed_instability_index >= 30 and Weigthed_instability_index <= 40:
-            return "Moderate"
-        else:
-            return "Weak"
-
-    def analyse_pI():
-        if abs((7- Weigthed_pI)) > 2:
-            return "Strong"
-        elif abs((7-Weigthed_pI)) >= 1 and abs((7-Weigthed_pI)) <= 2:
-            return "Moderate"
-        else:
-            return "Weak"
-        
-    cumulatif_sentence = ''
-    cumulatif_sentence += f'Overall, this protein is {analyse()}. '
-    if analyse_chain_length() == 'Weak':
-        cumulatif_sentence += f'Because of its small chain length, this protein could be denatured using mild heat or chaotropes. '
-    elif analyse_chain_length() == 'Moderate':
-        cumulatif_sentence += f'Because of its moderate chain length, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate combined with heat. '
+    #cumulative_sentence = ''
+    cumulative_sentence = f'Overall, this protein is {analysis}. '
+    if chain_length_analysis == 'Weak':
+        cumulative_sentence1 = f'Because of its small chain length, this protein could be denatured using mild heat or chaotropes. '
+    elif chain_length_analysis == 'Moderate':
+        cumulative_sentence1 = f'Because of its moderate chain length, this protein could be denatured using moderate to strong denaturants such as 6M urea, or sodium dodecyl sulfate combined with heat. '
     else:
-        cumulatif_sentence += 'Because of its long chain length, this protein is harder to denature. It is possible to denature it by using a combination of chaotropes, reducing agents and heat. '
+        cumulative_sentence1 = 'Because of its long chain length, this protein is harder to denature. It is possible to denature it by using a combination of chaotropes, reducing agents and heat. '
     #print(analyse_chain_length())
-    if analyse_molecular_weight() == 'Weak':
-        cumulatif_sentence += f'Because of its low mollecular weight, this protein could be denatured using mild heat or urea. '
-    elif analyse_molecular_weight() == 'Moderate':
-        cumulatif_sentence += f'Because of its moderate mollecular weight, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate combined with heat. '
+    if molecular_weight_analysis == 'Weak':
+        cumulative_sentence2 = f'Because of its low mollecular weight, this protein could be denatured using mild heat or urea. '
+    elif molecular_weight_analysis == 'Moderate':
+        cumulative_sentence2 = f'Because of its moderate mollecular weight, this protein could be denatured using moderate to strong denaturants such as 6M urea, or sodium dodecyl sulfate combined with heat. '
     else:
-        cumulatif_sentence += 'Because of its large mollecular weight, this protein is harder to denature. It is possible to denature it by using guanidium alongside a reducing agent. '
+        cumulative_sentence2 = 'Because of its large mollecular weight, this protein is harder to denature. It is possible to denature it by using guanidium alongside a reducing agent. '
     #print(analyse_molecular_weight())
-    if analyse_aromaticity() == 'Weak':
-        cumulatif_sentence += f'Because of its low aromaticity, this protein could be denatured using mild heat or a change in pH. '
-    elif analyse_aromaticity() == 'Moderate':
-        cumulatif_sentence += f'Because of its moderate aromaticity, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate. '
+    if aromaticity_analysis == 'Weak':
+        cumulative_sentence3 = f'Because of its low aromaticity, this protein could be denatured using mild heat or a change in pH. '
+    elif aromaticity_analysis == 'Moderate':
+        cumulative_sentence3 = f'Because of its moderate aromaticity, this protein could be denatured using moderate to strong denaturants such as 6M urea, or sodium dodecyl sulfate. '
     else:
-        cumulatif_sentence += 'Because of its strong aromaticity, this protein is harder to denature. It is possible to denature it by using guanidium alongside heat. '
+        cumulative_sentence3 = 'Because of its strong aromaticity, this protein is harder to denature. It is possible to denature it by using guanidium alongside heat. '
     #print(analyse_aromaticity())
-    if analyse_instability_index() == 'Weak':
-        cumulatif_sentence += f'Because of its high instability, this protein could be denatured using mild heat or a change in pH. '
-    elif analyse_instability_index() == 'Moderate':
-        cumulatif_sentence += f'Because of its moderate instability, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate. '
+    if instability_index_analysis == 'Weak':
+        cumulative_sentence4 = f'Because of its high instability, this protein could be denatured using mild heat or a change in pH. '
+    elif instability_index_analysis == 'Moderate':
+        cumulative_sentence4 = f'Because of its moderate instability, this protein could be denatured using moderate to strong denaturants such as 6M urea, or sodium dodecyl sulfate. '
     else:
-        cumulatif_sentence += 'Because of its low instability, this protein is harder to denature. It is possible to denature it by using guanidium alongside heat. '
+        cumulative_sentence4 = 'Because of its low instability, this protein is harder to denature. It is possible to denature it by using guanidium alongside heat. '
     #print(analyse_instability_index())
-    if analyse_pI() == 'Weak':
-        cumulatif_sentence += f'Because the isoelectric point is near the pH of 7, this protein could be denatured using mild heat or a change in pH. '
-    elif analyse_pI() == 'Moderate':
-        cumulatif_sentence += f'Because the isoelectric point is moderately near the pH of 7, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate. '
+    if pi_analysis == 'Weak':
+        cumulative_sentence5 = f'Because the isoelectric point is near the pH of 7, this protein could be denatured using mild heat or a change in pH. '
+    elif pi_analysis == 'Moderate':
+        cumulative_sentence5 = f'Because the isoelectric point is moderately near the pH of 7, this protein could be denatured using moderate to strong denaturants such as 6M urea, or sodium dodecyl sulfate. '
     else:
-        cumulatif_sentence += 'Because the isoelectric point is far from the pH of 7, this protein is harder to denature. It is possible to denature it by using chaotropes and sodium dodecyl sulfate. '
+        cumulative_sentence5 = 'Because the isoelectric point is far from the pH of 7, this protein is harder to denature. It is possible to denature it by using chaotropes and sodium dodecyl sulfate. '
     #print(analyse_pI())
-    if analyse() == 'Weak':
-        cumulatif_sentence += f'In summary, this protein is weak to denaturation. It should then be easy to break it using basic techniques such as mild heat, a change in pH or a compound such as urea. '
-    elif analyse() == 'Moderate':
-        cumulatif_sentence += f'In summary, this protein is moderately resistant to denaturation. It should then be easy to break it using moderate heat combined with a buffer to avoid aggregtion, using chemical denaturants such as sodium dodecyl sulfate or reducing agents if disulfide bonds are present within the protein. '
+    if analysis == 'Weak':
+        cumulative_sentence6 = f'To conclusion, this protein is weak to denaturation. It should then be easy to break it using basic techniques such as mild heat, a change in pH or a compound such as urea. '
+    elif analysis == 'Moderate':
+        cumulative_sentence6 = f'In conclusion, this protein is moderately resistant to denaturation. It should then be easy to break it using moderate heat combined with a buffer to avoid aggregation, using chemical denaturants such as sodium dodecyl sulfate or reducing agents if disulfide bonds are present within the protein. '
     else:
-        cumulatif_sentence += 'In summary, this protein is resistant to usual denaturation methods. We should then break it using more extreme techniques such as a combination of multiple methods. Exemples include combinations of strong chaotropic agents, detergents such as Sodium dodecyl sulfate, reducing agents, extreme heat or an adjustement to pH. '
+        cumulative_sentence6 = 'In conclusion, this protein is resistant to usual denaturation methods. We should then break it using more extreme techniques such as a combination of multiple methods. Exemples include combinations of strong chaotropic agents, detergents such as Sodium dodecyl sulfate, reducing agents, extreme heat or an adjustement to pH. '
     #print(analyse_pI())
     # Generate 3Dmol HTML viewer (built in function from the library)
     with open(pdb_path, 'r') as fh:
@@ -288,18 +199,28 @@ def view_pdb():
 
     view = py3Dmol.view(width=1000, height=500)
     view.addModel(pdb_text, 'pdb')
-    view.setStyle({'cartoon': {'color': 'spectrum'}})
-    view.setBackgroundColor("transparent")
+    view.setStyle({'cartoon': {'color': '#FFD580'}})  # base color
+    view.addStyle({'resi': list(range(1, 50))}, {'cartoon': {'color': "#FF6600"}})
+    view.addStyle({'resi': list(range(51, 100))}, {'cartoon': {'color': '#E25822'}})
+    view.addStyle({'resi': list(range(101, 150))}, {'cartoon': {'color': "#FF0000"}})
 
+    view.setBackgroundColor("#1E0064")
     view.zoomTo()
     viewer_html = view._make_html()
+    
     return render_template(
         'view.html',
         pdb_id=pdb_id,
         viewer_html=viewer_html,
         table_html=table_html,
         description=description,
-        cumulatif_sentence=cumulatif_sentence
+        cumulative_sentence=cumulative_sentence,
+        cumulative_sentence1=cumulative_sentence1,
+        cumulative_sentence2=cumulative_sentence2,
+        cumulative_sentence3=cumulative_sentence3,
+        cumulative_sentence4=cumulative_sentence4,
+        cumulative_sentence5=cumulative_sentence5,
+        cumulative_sentence6=cumulative_sentence6
     )
 
 ### Run Flask ###
--- ProteinVisualizer/old_app.py
+++ ProteinVisualizer/old_app.py
@@ -7,8 +7,6 @@ import py3Dmol
 import pandas as pd
 import requests
 
-from analysis_utils import AnalysisUtils
-
 app = Flask(__name__)
 
 ### Helper functions ###
@@ -118,76 +116,168 @@ def view_pdb():
         current_inst = df.loc[i]["instability_index"]
         current_length = df.loc[i]["length"]
         count_inst += current_inst * current_length
-    weighted_instability_index = count_inst/chain_length
+    Weigthed_instability_index = count_inst/chain_length
+
 
     count_aromaticity = 0
     for i in range(df.shape[0]):
         current_aromaticity = df.loc[i]["aromaticity"]
         current_length = df.loc[i]["length"]
         count_aromaticity += current_aromaticity * current_length
-    weighted_aromaticity = count_aromaticity/chain_length
+    Weigthed_aromaticity = count_aromaticity/chain_length
 
     count_pI = 0
     for i in range(df.shape[0]):
         current_pI = df.loc[i]["pI"]
         current_length = df.loc[i]["length"]
         count_pI += current_pI * current_length
-    weighted_pi = count_pI/chain_length
+    Weigthed_pI = count_pI/chain_length
 
-    analysis = AnalysisUtils.analyse(
-        chain_length, 
-        molecular_weight, 
-        weighted_aromaticity, 
-        weighted_instability_index, 
-        weighted_pi
-    )
-    
-    chain_length_analysis = AnalysisUtils.analyse_chain_length(chain_length)
-    molecular_weight_analysis = AnalysisUtils.analyse_molecular_weight(molecular_weight)
-    aromaticity_analysis = AnalysisUtils.analyse_aromaticity(weighted_aromaticity)
-    instability_index_analysis = AnalysisUtils.analyse_instability_index(weighted_instability_index)
-    pi_analysis = AnalysisUtils.analyse_pi(weighted_pi)
+
+    def analyse():
+        Strong = 0
+        Moderate = 0
+        Weak = 0
+
+        value = analyse_chain_length()
+        if value == "Strong":
+            Strong += 1
+        elif value == "Moderate":
+            Moderate += 1
+        else:
+            Weak += 1
+
+        value = analyse_molecular_weight()
+        if value == "Strong":
+            Strong += 1
+        elif value == "Moderate":
+            Moderate += 1
+        else:
+            Weak += 1
+        
+        value = analyse_aromaticity()
+        if value == "Strong":
+            Strong += 1
+        elif value == "Moderate":
+            Moderate += 1
+        else:
+            Weak += 1
+        
+        value = analyse_instability_index()
+        if value == "Strong":
+            Strong += 1
+        elif value == "Moderate":
+            Moderate += 1
+        else:
+            Weak += 1
+        
+        value = analyse_pI()
+        if value == "Strong":
+            Strong += 1
+        elif value == "Moderate":
+            Moderate += 1
+        else:
+            Weak += 1
+
+
+        if Strong > Moderate and Strong > Weak:
+            return "Strong"
+        elif Moderate > Strong and Moderate > Weak:
+            return "Moderate"
+        elif Weak > Strong and Weak > Moderate:
+            return "Weak"
+        elif Strong == Moderate and Strong > Weak:
+            return "Strong"
+        elif Strong == Weak and Strong > Moderate:
+            return "Strong"
+        elif Strong == Weak and Strong == Moderate:
+            return "Strong"
+        elif Moderate == Weak and Moderate > Strong:
+            return "Moderate"
+        else:
+            return "Strong"
+
+
+    def analyse_chain_length():
+        if chain_length > 400:
+            return "Strong"
+        elif chain_length >= 150 and chain_length <= 400:
+            return "Moderate"
+        else:
+            return "Weak"
+
+    def analyse_molecular_weight():
+        if molecular_weight > 60000:
+            return "Strong"
+        elif molecular_weight >= 20000 and molecular_weight <= 60000:
+            return "Moderate"
+        else:
+            return "Weak"
+
+    def analyse_aromaticity():
+        if Weigthed_aromaticity > 0.10:
+            return "Strong"
+        elif Weigthed_aromaticity >= 0.07 and Weigthed_aromaticity <= 0.10:
+            return "Moderate"
+        else:
+            return "Weak"
+
+    def analyse_instability_index():
+        if Weigthed_instability_index < 30:
+            return "Strong"
+        elif Weigthed_instability_index >= 30 and Weigthed_instability_index <= 40:
+            return "Moderate"
+        else:
+            return "Weak"
+
+    def analyse_pI():
+        if abs((7- Weigthed_pI)) > 2:
+            return "Strong"
+        elif abs((7-Weigthed_pI)) >= 1 and abs((7-Weigthed_pI)) <= 2:
+            return "Moderate"
+        else:
+            return "Weak"
         
     cumulatif_sentence = ''
-    cumulatif_sentence += f'Overall, this protein is {analysis}. '
-    if chain_length_analysis == 'Weak':
+    cumulatif_sentence += f'Overall, this protein is {analyse()}. '
+    if analyse_chain_length() == 'Weak':
         cumulatif_sentence += f'Because of its small chain length, this protein could be denatured using mild heat or chaotropes. '
-    elif chain_length_analysis == 'Moderate':
+    elif analyse_chain_length() == 'Moderate':
         cumulatif_sentence += f'Because of its moderate chain length, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate combined with heat. '
     else:
         cumulatif_sentence += 'Because of its long chain length, this protein is harder to denature. It is possible to denature it by using a combination of chaotropes, reducing agents and heat. '
     #print(analyse_chain_length())
-    if molecular_weight_analysis == 'Weak':
+    if analyse_molecular_weight() == 'Weak':
         cumulatif_sentence += f'Because of its low mollecular weight, this protein could be denatured using mild heat or urea. '
-    elif molecular_weight_analysis == 'Moderate':
+    elif analyse_molecular_weight() == 'Moderate':
         cumulatif_sentence += f'Because of its moderate mollecular weight, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate combined with heat. '
     else:
         cumulatif_sentence += 'Because of its large mollecular weight, this protein is harder to denature. It is possible to denature it by using guanidium alongside a reducing agent. '
     #print(analyse_molecular_weight())
-    if aromaticity_analysis == 'Weak':
+    if analyse_aromaticity() == 'Weak':
         cumulatif_sentence += f'Because of its low aromaticity, this protein could be denatured using mild heat or a change in pH. '
-    elif aromaticity_analysis == 'Moderate':
+    elif analyse_aromaticity() == 'Moderate':
         cumulatif_sentence += f'Because of its moderate aromaticity, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate. '
     else:
         cumulatif_sentence += 'Because of its strong aromaticity, this protein is harder to denature. It is possible to denature it by using guanidium alongside heat. '
     #print(analyse_aromaticity())
-    if instability_index_analysis == 'Weak':
+    if analyse_instability_index() == 'Weak':
         cumulatif_sentence += f'Because of its high instability, this protein could be denatured using mild heat or a change in pH. '
-    elif instability_index_analysis == 'Moderate':
+    elif analyse_instability_index() == 'Moderate':
         cumulatif_sentence += f'Because of its moderate instability, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate. '
     else:
         cumulatif_sentence += 'Because of its low instability, this protein is harder to denature. It is possible to denature it by using guanidium alongside heat. '
     #print(analyse_instability_index())
-    if pi_analysis == 'Weak':
+    if analyse_pI() == 'Weak':
         cumulatif_sentence += f'Because the isoelectric point is near the pH of 7, this protein could be denatured using mild heat or a change in pH. '
-    elif pi_analysis == 'Moderate':
+    elif analyse_pI() == 'Moderate':
         cumulatif_sentence += f'Because the isoelectric point is moderately near the pH of 7, this protein could be denatured using moderate to strong denaturants such as 6M urea or sodium dodecyl sulfate. '
     else:
         cumulatif_sentence += 'Because the isoelectric point is far from the pH of 7, this protein is harder to denature. It is possible to denature it by using chaotropes and sodium dodecyl sulfate. '
     #print(analyse_pI())
-    if analysis == 'Weak':
+    if analyse() == 'Weak':
         cumulatif_sentence += f'In summary, this protein is weak to denaturation. It should then be easy to break it using basic techniques such as mild heat, a change in pH or a compound such as urea. '
-    elif analysis == 'Moderate':
+    elif analyse() == 'Moderate':
         cumulatif_sentence += f'In summary, this protein is moderately resistant to denaturation. It should then be easy to break it using moderate heat combined with a buffer to avoid aggregtion, using chemical denaturants such as sodium dodecyl sulfate or reducing agents if disulfide bonds are present within the protein. '
     else:
         cumulatif_sentence += 'In summary, this protein is resistant to usual denaturation methods. We should then break it using more extreme techniques such as a combination of multiple methods. Exemples include combinations of strong chaotropic agents, detergents such as Sodium dodecyl sulfate, reducing agents, extreme heat or an adjustement to pH. '
@@ -199,7 +289,7 @@ def view_pdb():
     view = py3Dmol.view(width=1000, height=500)
     view.addModel(pdb_text, 'pdb')
     view.setStyle({'cartoon': {'color': 'spectrum'}})
-    view.setBackgroundColor("#000000") 
+    view.setBackgroundColor("transparent")
 
     view.zoomTo()
     viewer_html = view._make_html()
